cfc50b470368240fce9f343d2806bdc5504d5162
claudiacarion <claudia.carion@gmail.com>
2026-03-10 14:46:09 +0100

    change to X for open hamburger

--- src/components/Hamburger/index.jsx
+++ src/components/Hamburger/index.jsx
@@ -3,7 +3,7 @@ import styles from "./hamburger.module.css";
 const Hamburger = ({ isOpen, menuFunction }) => {
   return (
     <button className={styles.hamburger} onClick={() => menuFunction(isOpen)}>
-      <i className="fa-solid fa-bars"></i>
+      <i className={`fa-solid ${isOpen ? "fa-x" : "fa-bars"}`}></i>
     </button>
   );
 };
