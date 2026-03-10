8fa191c778f22a415695900d3f7bf3eb1f6dc100
claudiacarion <claudia.carion@gmail.com>
2026-03-10 14:12:01 +0100

    make hamburger 100 height

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -8,37 +8,37 @@ const Navigation = ({ updateFunction, isOpen, menuFunction }) => {
     <div className={styles.navWrapper}>
       <Hamburger menuFunction={toggleMenu} isOpen={isOpen} />
       <nav className={`${styles.navbar} ${isOpen ? styles.open : ""}`}>
-        <h3
+        <span
           className={styles.navBtn}
           onClick={() => {
             updateFunction(null);
             menuFunction(false);
           }}>
           Home
-        </h3>
-        <h3
+        </span>
+        <span
           className={styles.navBtn}
           onClick={() => {
             updateFunction("repeat");
             menuFunction(false);
           }}>
           On Repeat
-        </h3>
-        <h3
+        </span>
+        <span
           className={styles.navBtn}
           onClick={() => {
             updateFunction("discovery");
             menuFunction(false);
           }}>
           Discovery
-        </h3>
-        <h3
+        </span>
+        <span
           className={styles.navBtn}
           onClick={() => {
             (updateFunction("about"), menuFunction(false));
           }}>
           About
-        </h3>
+        </span>
       </nav>
     </div>
   );
